7a9fd09377ca83cd8329f95c556972f7cbee7d8d
skaladra <aleksey.komissarov@saritasa.com>
2022-04-24 00:54:44 +0500

    Add get posts command and Automapper

---
+++ src/AskMe.Web/AskMe.UseCases/User/GetPostsCreatedByUser/GetPostsCreatedByUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using AskMe.UseCases.Common.Dtos.Post;
+using MediatR;
+
+namespace AskMe.UseCases.User.GetPostsCreatedByUser;
+
+public record GetPostsCreatedByUserCommand(Guid Id) : IRequest<IEnumerable<PostDto>>;
---
+++ src/AskMe.Web/AskMe.UseCases/User/UserMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AskMe.Domain.Posts.Entities;
+using AskMe.Domain.Users.Entities;
+using AskMe.UseCases.Common.Dtos.Post;
+using AskMe.UseCases.Common.Dtos.User;
+using AutoMapper;
+
+namespace AskMe.UseCases.User;
+
+public class UserMappingProfile : Profile
+{
+    public UserMappingProfile()
+    {
+        CreateMap<Post, PostDto>().ReverseMap();
+        CreateMap<ApplicationUser, UserDto>().ReverseMap();
+    }
+}
