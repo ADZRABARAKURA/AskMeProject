08b2eecabae83db12780e51c48f901e3ed12d394
Aleksey Komissarov <aleksey.komissarov@saritasa.com>
2022-06-13 13:47:04 +0500

    Login window improvements

---
+++ src/AskMe.Desktop/AskMe.Desktop/AskMe.Desktop/ViewModels/DonateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AskMe.Desktop.ViewModels;
+
+public class DonateViewModel
+{
+    public string Header { get; init; }
+    public string Text { get; init; }
+}
